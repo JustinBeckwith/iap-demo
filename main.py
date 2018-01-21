24e901141255acb935d07e96c57036bb0808c016
Justin Beckwith <beckwith@google.com>
2018-01-20 17:57:58 -0800

    return json

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from google.appengine.api import users
 import webapp2
+import json
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
         if user:
-            nickname = user.nickname()
-            logout_url = users.create_logout_url('/')
-            greeting = 'Welcome, {}! (<a href="{}">sign out</a>)'.format(
-                nickname, logout_url)
+            self.response.headers['Content-Type'] = 'application/json'
+            res = { 'nickname': user.nickname() }
+            self.response.out.write(json.dumps(res))
         else:
             login_url = users.create_login_url('/')
             greeting = '<a href="{}">Sign in</a>'.format(login_url)
-        self.response.write('<html><body>{}</body></html>'.format(greeting))
+            self.response.write('<html><body>{}</body></html>'.format(greeting))
 
 app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
