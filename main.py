20b64a8decd32a3bd4e3a2c12366203cf87c2f21
Justin Beckwith <beckwith@google.com>
2018-01-20 17:51:17 -0800

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from google.appengine.api import users
+import webapp2
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        if user:
+            nickname = user.nickname()
+            logout_url = users.create_logout_url('/')
+            greeting = 'Welcome, {}! (<a href="{}">sign out</a>)'.format(
+                nickname, logout_url)
+        else:
+            login_url = users.create_login_url('/')
+            greeting = '<a href="{}">Sign in</a>'.format(login_url)
+        self.response.write('<html><body>{}</body></html>'.format(greeting))
+
+app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
